bc77a1ed652d22abae5de294f030682a0c8446c0
Vladimir Legeza <vladimir.legeza@gmail.com>
2012-03-26 02:16:27 +0400

    Add: setup.py installer Change: hglock.py rights Fix: unable to unlock if no files specified Add: Handler erros in case a file is outside the repo. Add: Example of hglock.rc Change: Hook handlers a reoganized Fix: Locking over the hook if repository is located in a filesystem root.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+  name='hglock-le',
+  version='0.1',
+  description='File locking mechanizm for Mercurial',
+  author='Vladimir Legeza',
+  author_email='vladimir.legeza@gmail.com',
+  url='https://bitbucket.org/legeza/hglock-le',
+  keywords='hg lock mercurial',
+  py_modules=['hglock',]
+  )
